ea717d1782277004df314700b746a9361f330c4d
Fumito Hamamura <fumito.ham@gmail.com>
2017-08-11 21:39:19 +0900

    added a pickling test

--- modelx/tests/core/test_model_pickle.py
+++ modelx/tests/core/test_model_pickle.py
@@ -1,24 +1,58 @@
+from textwrap import dedent
 import pytest
+import pickle
 
 from modelx.core.api import *
 from modelx.core.cells import CellPointer
 from modelx.core.base import get_interfaces
 
 
-@pytest.fixture
-def simplemodel():
+# ---- Test impl ----
 
-    model = create_model()
+@pytest.fixture
+def pickletest():
+
+    model = create_model('TestModel')
     space = model.create_space()
 
-    @defcells
-    def fibo(x):
-        if x == 0 or x == 1:
-            return x
-        else:
-            return fibo(x - 1) + fibo(x - 2)
+    func1 = dedent("""\
+    def single_value(x):
+        return 5 * x
+    """)
 
-    return model
+    func2 = dedent("""\
+    def mult_single_value(x):
+        return 2 * single_value(x)
+    """)
+
+    func1 = space.create_cells(func=func1)
+    func2 = space.create_cells(func=func2)
+
+    func2(5)
+
+    byte_obj = pickle.dumps(model._impl)
+    unpickled = pickle.loads(byte_obj)
+
+    return [model._impl, unpickled]
+
+
+def test_unpickled_model(pickletest):
+
+    model, unpickeld = pickletest
+
+    errors = []
+
+    if not model.name == unpickeld.name:
+        errors.append("name did not match")
+
+    if not hasattr(model, 'interface'):
+        errors.append("no interface")
+
+    if not hasattr(model, 'cellgraph'):
+        errors.append("no cellgraph")
+
+    assert not errors, "errors:\n{}".format("\n".join(errors))
+
 
 
 def x_test_model_load(simplemodel):
@@ -31,3 +65,5 @@ def x_test_model_load(simplemodel):
     # space = simplemodel.currentspace
     # simplemodel.save('data\simplemodel')
     # print(space.fibo[10])
+
+
