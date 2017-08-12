09cd55337ca0a93a2bb29821ce9f5b8df8235522
Fumito Hamamura <fumito.ham@gmail.com>
2017-08-12 13:05:32 +0900

    moved sys.setrecursionlimit to not expose sys

--- modelx/__init__.py
+++ modelx/__init__.py
@@ -1,7 +1,5 @@
-import sys
+
 
 VERSION = (0, 0, 1)
-
-sys.setrecursionlimit(100000)
 
 from modelx.core.api import *
--- modelx/core/__init__.py
+++ modelx/core/__init__.py
@@ -1,5 +1,9 @@
+import sys
+
 from modelx.core.system import System
 
+
+sys.setrecursionlimit(100000)
 system = System()
 
 
