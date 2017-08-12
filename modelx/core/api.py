8b1a81b96e402564bc41d362048aaded04dec85d
Fumito Hamamura <fumito.ham@gmail.com>
2017-08-12 13:08:21 +0900

    renamed some global vars with _ to not expose them

--- modelx/core/api.py
+++ modelx/core/api.py
@@ -1,52 +1,52 @@
-from types import FunctionType
+from types import FunctionType as _FunctionType
 
-from modelx.core import system
-from modelx.core.cells import CellsMaker
+from modelx.core import system as _system
+from modelx.core.cells import CellsMaker as _CellsMaker
 
 
 def create_model(name=None):
-    return system.create_model(name).interface
+    return _system.create_model(name).interface
 
 
 def defcells(space=None, name=None):
 
-    if isinstance(space, FunctionType) and name is None:
+    if isinstance(space, _FunctionType) and name is None:
         # called as a function decorator
         func = space
-        return system.currentspace.create_cells(func=func).interface
+        return _system.currentspace.create_cells(func=func).interface
 
     else:
         # return decorator itself
         if not space:
-            space = system.currentspace.interface
+            space = _system.currentspace.interface
 
-        return CellsMaker(space=space._impl, name=name)
+        return _CellsMaker(space=space._impl, name=name)
 
 
 def create_cells_from_module(module, space=None):
-    return system.create_cells_from_module(module, space=None)
+    return _system.create_cells_from_module(module, space=None)
 
 
 def get_models():
-    return system.models
+    return _system.models
 
 
 def get_currentmodel():
-    return system.currentmodel.interface
+    return _system.currentmodel.interface
 
 
 def get_currentspace():
-    return system.currentmodel.currentspace.interface
+    return _system.currentmodel.currentspace.interface
 
 
 def get_self():
     """Return self space during its dynamic creation in factory."""
 
-    return system.self.interface
+    return _system.self.interface
 
 
 def open_model(path):
-    return system.open_model(path)
+    return _system.open_model(path)
 
 
 
