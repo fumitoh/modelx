a39c4b7ed819b22ab0f517a3109fe97cf8e9e6ed
Fumito Hamamura <fumito.ham@gmail.com>
2017-10-22 00:20:01 +0900

    License changed to LGPLv3 and copyright disclaimer added

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from codecs import open
 from os import path
 
 DISTNAME = 'modelx'
-LICENSE = 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+LICENSE = 'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
 AUTHOR = "Fumito Hamamura"
 EMAIL = "fumito.ham@gmail.com"
 URL = "https://github.com/fumitoh/modelx"
@@ -73,7 +73,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
