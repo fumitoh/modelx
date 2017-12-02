9a14ffc4295657e5a32033c236472e17350515ab
Fumito Hamamura <fumito.ham@gmail.com>
2017-12-03 00:54:04 +0900

    FIX: Removed redundant console script from setup.py

--- setup.py
+++ setup.py
@@ -148,8 +148,8 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
+        # 'console_scripts': [
+        #     'sample=sample:main',
+        # ],
     },
 )
