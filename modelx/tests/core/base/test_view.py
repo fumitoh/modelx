5604be99f9abea82023e169734728700147275d2
Fumito Hamamura <fumito.ham@gmail.com>
2018-06-07 23:50:23 +0900

    TST: Selected view

---
+++ modelx/tests/core/base/test_view.py
@@ -0,0 +1,31 @@
+import modelx as mx
+import pytest
+
+
+@pytest.fixture
+def viewtest():
+    m, s = mx.new_model(), mx.new_space()
+
+    @mx.defcells
+    def foo(x):
+        return 2 * x
+
+    @mx.defcells
+    def bar(y):
+        return foo(y) * y
+
+    @mx.defcells
+    def baz(z):
+        return bar(z) * 3
+
+    return s
+
+
+def test_view(viewtest):
+
+    selected = viewtest.cells['foo', 'bar']
+
+    assert len(selected) == 2
+
+    for name, cells in selected.items():
+        assert viewtest.cells[name] is cells
