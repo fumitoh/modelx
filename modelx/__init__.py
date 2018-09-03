ed6d7ed43ac8660e9cc37e8071b2cdf0857db609
Fumito Hamamura <fumito.ham@gmail.com>
2018-09-03 22:15:39 +0900

    DIST: Bump version back to dev

--- modelx/__init__.py
+++ modelx/__init__.py
@@ -14,7 +14,7 @@
 
 
 
-VERSION = (0, 0, 14)
+VERSION = (0, 0, 15, 'dev')
 __version__ = '.'.join([str(x) for x in VERSION])
 from modelx.core.api import *  # must come after __version__ assignment.
 from modelx.qtgui.api import *
