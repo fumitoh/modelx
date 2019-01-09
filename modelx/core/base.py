31286ff8d9c46636dfbd77951e353deeb2721fc5
Fumito Hamamura <fumito.ham@gmail.com>
2019-01-10 00:09:38 +0900

    ENH: Exclude items with _ prefix in BaseView

--- modelx/core/base.py
+++ modelx/core/base.py
@@ -810,7 +810,8 @@ class BaseView(Mapping):
         result = {'type': type(self).__name__}
         try:
             result['items'] = {name: item._baseattrs
-                               for name, item in self.items()}
+                               for name, item in self.items()
+                               if name[0] != '_'}
         except:
             raise RuntimeError("%s literadict raised an error" % self)
 
