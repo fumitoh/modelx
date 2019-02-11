8bae6dc0cf74069303afea7be02ff48d914d8cab
Fumito Hamamura <fumito.ham@gmail.com>
2019-02-11 18:01:52 +0900

    TST: Add test_signature

---
+++ modelx/tests/core/cells/test_signature.py
@@ -0,0 +1,87 @@
+import modelx as mx
+from modelx import defcells
+import pytest
+
+
+@pytest.fixture
+def cells_signatures():
+
+    model = mx.new_model('Model')
+    space = model.new_space('Space')
+
+    @defcells
+    def no_param():
+        return 1
+
+    @defcells
+    def single_param(x):
+        return x
+
+    @defcells
+    def single_param_with_default(x=1):
+        return x
+
+    @defcells
+    def mult_params(x, y):
+        return x + y
+
+    @defcells
+    def mult_params_with_default(x, y, z=3):
+        return sum([x, y, z])
+
+    yield space
+    model.close()
+
+
+def test_get_too_many_args(cells_signatures):
+    
+    space = cells_signatures
+
+    with pytest.raises(TypeError):
+        space.no_param(1)
+
+    with pytest.raises(TypeError):
+        space.no_param[1]
+
+    with pytest.raises(TypeError):
+        space.single_param(1, 2)
+
+    with pytest.raises(TypeError):
+        space.single_param[1, 2]
+
+    with pytest.raises(TypeError):
+        space.single_param[(1, 2)] # Intepreted as [1, 2]
+
+
+def test_get_too_few_args(cells_signatures):
+
+    space = cells_signatures
+
+    with pytest.raises(TypeError):
+        space.single_param()
+
+    with pytest.raises(TypeError):
+        space.mult_params(1)
+
+    with pytest.raises(TypeError):
+        space.mult_params[1]
+
+
+def test_get_default_args(cells_signatures):
+
+    space = cells_signatures
+    assert space.single_param_with_default() == 1
+    assert space.mult_params_with_default(1, 2) == 6
+    assert space.mult_params_with_default[1, 2] == 6
+    assert space.mult_params_with_default[(1, 2)] == 6
+    assert space.mult_params_with_default[[1, 2]] == 6
+
+
+def test_getitem_string(cells_signatures):
+    
+    space = cells_signatures
+    assert space.single_param["foo"] == "foo"
+    assert space.single_param_with_default["bar"] == "bar"
+    assert space.mult_params["foo", "bar"] == "foobar"
+    assert space.mult_params[("foo", "bar")] == "foobar"
+    assert space.mult_params[["foo", "bar"]] == "foobar"
