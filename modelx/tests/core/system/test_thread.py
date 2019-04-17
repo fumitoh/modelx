2d292620dc67d8d66bf92bdfbdcccd7988f1201e
Fumito Hamamura <fumito.ham@gmail.com>
2019-04-17 19:56:42 +0900

    TST: Additional test

--- modelx/tests/core/system/test_thread.py
+++ modelx/tests/core/system/test_thread.py
@@ -1,5 +1,6 @@
 import modelx as mx
-
+from modelx.core.errors import DeepReferenceError
+import pytest
 
 def test_max_recursion():
 
@@ -15,3 +16,18 @@ def test_max_recursion():
 
     assert foo(maxdepth) == maxdepth
 
+
+def test_maxout_recursion():
+
+    maxdepth = 65001
+    m, s = mx.new_model(), mx.new_space()
+
+    @mx.defcells
+    def foo(x):
+        if x == 0:
+            return 0
+        else:
+            return foo(x-1) + 1
+
+    with pytest.raises(DeepReferenceError):
+        foo(maxdepth)
