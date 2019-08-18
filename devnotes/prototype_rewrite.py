19486c6aa5122798c630810be03a53d1a226b01a
Fumito Hamamura <fumito.ham@gmail.com>
2019-08-18 23:22:39 +0900

    DSGN: Update prototype_rewrite

--- devnotes/prototype_rewrite.py
+++ devnotes/prototype_rewrite.py
@@ -1,75 +1,177 @@
-
+import itertools
 import networkx as nx
 
 
 class SpaceGraph(nx.DiGraph):
 
+    def get_mro(self, node):
+        """Calculate the Method Resolution Order of bases using the C3 algorithm.
+
+        Code modified from
+        http://code.activestate.com/recipes/577748-calculate-the-mro-of-a-class/
+
+        Args:
+            bases: sequence of direct base spaces.
+
+        Returns:
+            mro as a list of bases including node itself
+        """
+        seqs = [self.get_mro(base)
+                for base in self.predecessors(node)
+                ] + [list(self.predecessors(node))]
+        res = []
+        while True:
+            non_empty = list(filter(None, seqs))
+
+            if not non_empty:
+                # Nothing left to process, we're done.
+                res.insert(0, node)
+                return res
+
+            for seq in non_empty:  # Find merge candidates among seq heads.
+                candidate = seq[0]
+                not_head = [s for s in non_empty if candidate in s[1:]]
+                if not_head:
+                    # Reject the candidate.
+                    candidate = None
+                else:
+                    break
+
+            if not candidate:  # Better to return None instead of error?
+                raise TypeError(
+                    "inconsistent hierarchy, no C3 MRO is possible"
+                )
+
+            res.append(candidate)
+
+            for seq in non_empty:
+                # Remove candidate.
+                if seq[0] == candidate:
+                    del seq[0]
+
+    def derive_edges(self):
+        for e in self.visit_edges():
+            self.derive_tree(e)
+
+    def get_absbases(self):
+        result = set(self.edges)
+        for e in self.edges:
+            tail, head = e
+            if self.get_endpoints(
+                    self.get_treenodes(
+                        self.get_topnode(tail)), edge="in"):
+                result.remove(e)
+
+        return result
+
+    def visit_edges(self):
+        """Generator yielding edges in breadth-first order"""
+        que = list(self.get_absbases())
+        visited = set()
+        while que:
+            e = que.pop(0)
+            yield e
+
+            visited.add(e)
+            _, head = e
+            edges = self.out_edges(
+                self.get_endpoints(
+                    self.get_treenodes(
+                        self.get_topnode(head)
+                    ), edge="out"))
+
+            que.extend(e for e in edges if e not in visited)
+
+    def derive_tree(self, edge):
+        tail, head = edge
+
+        tail_len = len(tail.split("."))
+        head_len = len(head.split("."))
+
+        debug = self.get_treenodes(tail, include_self=False)
+        # Remove parent
+        bases = set(".".join(n.split(".")[tail_len:])
+                    for n in self.get_treenodes(tail, include_self=False))
+        subs = set(".".join(n.split(".")[head_len:])
+                   for n in self.get_treenodes(head, include_self=False))
+
+        # missing = bases - subs
+        derived = set((tail + "." + n, head + "." + n) for n in bases)
+
+        self.add_edges_from(derived, type="derived")
+
     def get_subgraph(self, node):
-        """"""
-        # Get top nodes and explicit edges
-        nodes = set().union(self.get_inherited(node, "sub"),
-                            self.get_inherited(node, "base"))
 
-        explg = self.subgraph(nodes).copy()
-        implg = explg.copy()
+        nodes, _ = self.get_nodeset(node, set())
+        return type(self).copy(self.subgraph(nodes))
 
-        # Bring child nodes in and add implicit edges
-        for base, sub in explg.edges:
-            self.derive_children(implg, base, sub)
+    def get_nodeset(self, node, processed):
+        """Get a subset of self.
 
-        return implg
+        Get a subset of self such that the subset contains
+        nodes connected to ``node`` either through inheritance or composition.
 
-    def derive_children(self, g: nx.DiGraph, base, sub):
-        g.add_edge(base, sub)
-        for node in self.get_child_nodes(base):
-            name = node.split(".")[-1]
-            derived = ".".join([sub, name])
-            self.derive_children(g, node, derived)
+        0. Prepare an emptly node set
+        1. Get the top endopoint in the tree that ``node`` is in, or ``node``
+           if none.
+        2. Add to the node set all the child nodes of the top endpoint.
+        3. Find node sets.
+        4. For each endpoint in the child nodes, repeat from 1.
+        """
 
-    def get_inherited(self, origin, direction="sub"):
-        """Get base and sub nodes from name"""
-        if direction == "sub":
-            edge = self.out_edges
-            method = self.successors
-        elif direction == "base":
-            edge = self.in_edges
-            method = self.predecessors
+        top = self.get_topnode(node)
+        tree = self.get_treenodes(top)
+        ends = self.get_endpoints(tree)
+
+        neighbors = self.get_otherends(ends) - processed
+        processed.update(ends)
+        result = tree.copy()
+        for n in neighbors:
+            ret_res, _ = self.get_nodeset(n, processed)
+            result.update(ret_res)
+
+        return result, processed
+
+    def get_parent_nodes(self, node: str, include_self=True):
+        split = node.split(".")
+        size = len(split) if include_self else len(split) - 1
+        return [".".join(split[:i + 1]) for i in range(len(split))][:size]
+
+    def get_topnode(self, node):
+        parents = self.get_parent_nodes(node)
+        return next((n for n in parents if self.is_endpoint(n)), node)
+
+    def get_treenodes(self, node, include_self=True):
+        result = set([node]) if include_self else set()
+        return result.union(
+            n for n in self.nodes if n[:len(node) + 1] == (node + "."))
+
+    def get_endpoints(self, nodes, edge="any"):
+        return set(n for n in nodes if self.is_endpoint(n, edge))
+
+    def get_otherends(self, nodes, edge="any"):
+        l = [set(self.get_neighbors(n, edge)) for n in nodes]
+        return set().union(*l)
+
+    def get_neighbors(self, node, edge):
+        if edge == "in":
+            return self.predecessors(node)
+        elif edge == "out":
+            return self.successors(node)
+        else:
+            return itertools.chain(
+                self.predecessors(node), self.successors(node))
+
+    def is_endpoint(self, node, edge="any"):
+        if edge == "out":
+            return bool(self.out_edges(node))
+        elif edge == "in":
+            return bool(self.in_edges(node))
+        elif edge == "any":
+            return bool(self.out_edges(node) or
+                        self.in_edges(node))
         else:
             raise ValueError
-
-        trees = self.get_lineage(origin, edge)
-        result = trees.copy()
-        for top in trees:
-            next_tops = set(node for node in method(top))
-            result.update(next_tops)
-            for node in next_tops:
-                result.update(self.get_inherited(node, direction))
-        return result
-
-    def get_lineage(self, node, edge):
-        return set().union(self.get_parents(node, edge),
-                           self.get_children(node, edge))
-
-    def get_children(self, node, edge):
-        result = set()
-        if edge(node):
-            result.add(node)
-        for child in self.get_child_nodes(node):
-            result.update(self.get_children(child, edge))
-        return result
-
-    def get_parents(self, node, edge):
-        return set(parent for parent in
-                   self.get_parent_nodes(node) if edge(parent))
-
-    def get_parent_nodes(self, node: str):
-        split = node.split(".")
-        return [".".join(split[:i+1]) for i in range(len(split))]
-
-    def get_child_nodes(self, node: str):
-        space = self.nodes[node]["space"]
-        return [child.get_fullname(omit_model=True)
-                for child in space.spaces.values()]
 
     def add_bases(self, space, *bases):
         space_name = space.get_fullname(omit_model=True)
@@ -90,29 +192,46 @@ class SpaceGraph(nx.DiGraph):
 if __name__ == "__main__":
     import modelx as mx
     from modelx.core.base import get_impls
+
     g = SpaceGraph()
 
     """
-    A---B---C
-        |
-    +---+
-    |
-    D
-    
+            2
+         C<---------F
+         |          |
+       1 |          |
+    A<---B          B
+         |  3       |
+         D<---E     D
+              |
+              G
     """
 
     m = mx.new_model()
-    m.new_space("A").new_space("B") # .new_space("C")
-    g.add_spaces(*get_impls([m.A, m.A.B])) #, m.A.B.C]))
-    # g.get_compos("A.B")
-    m.new_space("D")
-    g.add_spaces(m.D._impl)
-    g.add_bases(m.D._impl, m.A.B._impl)
-    result = g.get_inherited("A.B")
-    sg = g.get_subgraph("A")
-    # m.new_space("AA")
-    # m.A.add_bases(m.AA)
-    m.new_space("AA").new_space("B").new_space("C")
-    g.add_bases(m.A._impl, m.AA._impl)
-    g.add_spaces(m.AA.B._impl, m.AA.B.C._impl)
-    sg = g.get_subgraph("A")
+    m.new_space("A")
+    m.new_space("C").new_space("B").new_space("D")
+    m.new_space("E").new_space("G")
+    m.new_space("F").new_space("B").new_space("D")
+
+    g.add_spaces(
+        *get_impls(
+            [m.A, m.C, m.C.B, m.C.B.D, m.F, m.F.B, m.F.B.D, m.E, m.E.G]))
+    g.add_bases(m.A._impl, m.C.B._impl)
+    g.add_bases(m.C._impl, m.F._impl)
+    g.add_bases(m.C.B.D._impl, m.E._impl)
+    retult, _ = g.get_nodeset("A", set())
+
+    nodes = ["A", "C", "C.B", "C.B.D", "E", "E.G", "F", "F.B", "F.B.D"]
+    for n in nodes:
+        print(g.get_nodeset(n, set())[0])
+
+    print(g.get_nodeset("C.B", set())[0])
+    sg = g.get_subgraph("C.B")
+
+    g.get_nodeset("F.B", set())[0]
+    sg.derive_edges()
+
+    for e in sg.edges:
+        print(e, sg.edges[e])
+
+    print(sg.get_mro('C.B.D'))
