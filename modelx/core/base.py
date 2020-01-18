3030a6102e4b2ed02f81a39990048d73713aa693
Fumito Hamamura <fumito.ham@gmail.com>
2020-01-18 14:46:50 +0900

    FAC: Remove comment out

--- modelx/core/base.py
+++ modelx/core/base.py
@@ -836,14 +836,6 @@ class BoundFunction(LazyEval):
         name = func.__name__  # self.cells.name   # func.__name__
         namespace_impl = self.owner._namespace_impl.refresh
         namespace = namespace_impl.interfaces
-        selfnode = get_node(self.owner, None, None)
-
-        # for name in self.owner.formula.srcnames:
-        #     if name in namespace_impl and isinstance(
-        #         namespace_impl[name], ReferenceImpl
-        #     ):
-        #         refnode = get_node(namespace_impl[name], None, None)
-        #         self.owner.model.lexdep.add_path([selfnode, refnode])
 
         closure = func.__closure__  # None normally.
         if closure is not None:  # pytest fails without this.
