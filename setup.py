4b28ffa84f919f9e15c4f76b06efd10ac5f27aa0
Fumito Hamamura <fumitoh@users.noreply.github.com>
2020-12-07 22:24:43 +0900

    DIST: Add Python 3.9 in supported version list

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
