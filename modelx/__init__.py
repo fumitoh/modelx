f347b140338aa4ce3d45b20f9167479a0c0ef77f
Fumito Hamamura <fumito.ham@gmail.com>
2021-02-06 20:14:46 +0900

    DIST: Bump version back to dev

--- modelx/__init__.py
+++ modelx/__init__.py
@@ -20,7 +20,7 @@ Attributes:
 
 """
 
-VERSION = (0, 12, 1)
+VERSION = (0, 13, 0, "dev")
 __version__ = ".".join([str(x) for x in VERSION])
 from modelx.core.api import *  # must come after __version__ assignment.
 try:
