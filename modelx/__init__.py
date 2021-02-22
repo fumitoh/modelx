04a0c181a09d5576a44e1635ce989da3470fed6a
Fumito Hamamura <fumito.ham@gmail.com>
2021-02-23 01:51:02 +0900

    DIST: Release v0.13.0

--- modelx/__init__.py
+++ modelx/__init__.py
@@ -20,7 +20,7 @@ Attributes:
 
 """
 
-VERSION = (0, 13, 0, "dev")
+VERSION = (0, 13, 0)
 __version__ = ".".join([str(x) for x in VERSION])
 from modelx.core.api import *  # must come after __version__ assignment.
 try:
