27c3a866b1e53396318d14defefacfe21c31bb8b
Fumito Hamamura <fumitoh@users.noreply.github.com>
2021-05-15 01:19:02 +0900

    Update system.py

--- modelx/core/system.py
+++ modelx/core/system.py
@@ -46,7 +46,7 @@ class Executor:
         self.thread = Executor.ExecThread(self)
         self.thread.daemon = True
         if sys.platform == "darwin":
-            last_size = threading.stack_size(0xFFFFFF)
+            last_size = threading.stack_size(0x7FFFFF)
         else:
             last_size = threading.stack_size(0xFFFFFFF)
         self.thread.start()
