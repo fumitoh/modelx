5129fb8cb085b188d6dd8a6ce8e78bca3912f0eb
Fumito Hamamura <fumito.ham@gmail.com>
2024-07-27 15:32:34 +0900

    TST: Move test_new_cells in tests/space

--- modelx/tests/core/space/test_new_cells.py
+++ modelx/tests/core/space/test_new_cells.py
@@ -63,3 +63,21 @@ def test_new_cells_order():
 
     m._impl._check_sanity()
     m.close()
+
+
+def test_new_cells_on_base_when_a_sub_has_it():
+    """
+        Base---Sub1---foo
+         +-foo  |
+                +-Sub2
+
+        https://github.com/fumitoh/modelx/issues/138
+    """
+    m = mx.new_model()
+    base = m.new_space('Base')
+    sub1 = m.new_space('Sub1', bases=base)
+    sub2 = m.new_space('Sub2', bases=base)
+
+    sub1.new_cells('foo')
+    base.new_cells('foo')
+    assert 'foo' in sub2
