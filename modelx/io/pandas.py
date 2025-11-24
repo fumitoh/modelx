5f183ade868663932318b9ae6c125a1741d54760
Fumito Hamamura <fumito.ham@gmail.com>
2025-11-24 15:33:49 +0900

    DOC: Update copyright year to 2025 in multiple files

--- modelx/io/pandas.py
+++ modelx/io/pandas.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2024 Fumito Hamamura <fumito.ham@gmail.com>
+# Copyright (c) 2017-2025 Fumito Hamamura <fumito.ham@gmail.com>
 
 # This library is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
